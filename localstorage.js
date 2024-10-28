2e0b63eb659d7e486fda0c742ebaebfe4f29fa7b
Carol <162912256+carolmarques09@users.noreply.github.com>
2024-10-28 15:42:42 -0300

    Add files via upload

---
+++ localstorage.js
@@ -0,0 +1,9 @@
+const name = 'valor';
+
+localStorage.setItem('valor', name)
+
+console.log(localStorage.getItem('valor'));
+
+localStorage.setItem('pessoa', JSON.stringify({name: 'cliente'}));
+
+console.log(JSON.parse(localStorage.getItem('name')));
